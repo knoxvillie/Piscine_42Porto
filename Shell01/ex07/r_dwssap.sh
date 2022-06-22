d5f705ea7cea7f4daaba09e17240a017e50adc8b
Kelvin Faustino <kelvinfaustino@gmail.com>
2022-06-22 18:54:20 +0100

    ex07

--- Shell01/ex07/r_dwssap.sh
+++ Shell01/ex07/r_dwssap.sh
@@ -1,1 +1,2 @@
-cat /etc/passwd | sed '/^#/d' | awk 'NR % 2 == 0 {print $0}' | awk -F ":" '{  print $1 }' | rev | sort -fnr | awk -v FT_LINE1="$FT_LINE1" -v FT_LINE2="$FT_LINE2"  'NR >= FT_LINE1 && NR <= FT_LINE2 { print $0 }' | tr '\n' ' '  | sed 's+ +, +g' | sed 's+, $+.+g' | tr -d '\n'
+#!/bin/sh
+cat /etc/passwd | cut -d':' -f 1 | sed -n 'n;p' | rev | sort -r | tr -s '\n' ',' | sed 's/,/, /g' |  sed 's/, $/./' 
