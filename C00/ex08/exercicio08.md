0a962ddcd00dfe73f0518092c45867277dc8f27c
Kelvin Faustino <kelvinfaustino@gmail.com>
2022-07-07 09:03:50 -0300

    c00

---
+++ C00/ex08/exercicio08.md
@@ -0,0 +1,17 @@
+# C 00 Exercício 08
+---
+
+- Escreva uma função que mostre todas as diferentes combinações de `n` números em ordem crescente.
+
+- `n` será tal que: 0 < n < 10.
+```
+$>./a.out | cat -e
+01, 02, 03, ..., 09, 12, ..., 79, 89$>
+```
+
+- Ela deve ser prototipada da seguinte maneira:
+```
+void ft_print_comb(int n);
+```
+---
+---
--- C00/ex08/ft_print_combn.c
+++ C00/ex08/ft_print_combn.c
@@ -8,10 +8,26 @@ void ft_putchar(char c){
 }
 
 void ft_print_combn(int n){
+    int i, j, e;
+    int digito = 1;
+
+    for (e=0; e<n; e++){
+        digito = digito * 10;
+    }
+    for (i=0; i<digito; i++){
+        for (j=i+1; j<10; j++){
+            ft_putchar(i + '0');
+            ft_putchar(j + '0');
+            ft_putchar(',');
+            ft_putchar(' ');
+
+        }
+
+    }
   
 }
 
-int main(void){
-    ft_print_combn(3);
+int main(){
+    ft_print_combn(1);
     return 0;
 }
